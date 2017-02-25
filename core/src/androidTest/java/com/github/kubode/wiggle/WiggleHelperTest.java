e301d7c04741a6a22d679dee796c755dd7d75f6c
kubode <kubode@team-lab.com>
2017-02-25 22:55:35 +0900

    Add inRoot. https://google.github.io/android-testing-support-library/docs/espresso/advanced/index.html#using-inroot-to-target-non-default-windows

--- core/src/androidTest/java/com/github/kubode/wiggle/WiggleHelperTest.java
+++ core/src/androidTest/java/com/github/kubode/wiggle/WiggleHelperTest.java
@@ -16,10 +16,13 @@ import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.swipeDown;
 import static android.support.test.espresso.action.ViewActions.swipeUp;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static com.github.kubode.wiggle.MyMatchers.withLastNon0TranslationY;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -35,13 +38,17 @@ public class WiggleHelperTest {
 
     @Test
     public void lastNon0TranslationYGreaterThan0WhenSwipeUp() {
-        onView(withId(R.id.scrollView)).perform(swipeUp());
+        onView(withId(R.id.scrollView))
+                .inRoot(withDecorView(not(is(rule.getActivity().getWindow().getDecorView()))))
+                .perform(swipeUp());
         onView(withId(R.id.view)).check(matches(withLastNon0TranslationY(greaterThan(0f))));
     }
 
     @Test
     public void lastNon0TranslationYLessThan0WhenSwipeDown() {
-        onView(withId(R.id.scrollView)).perform(swipeDown());
+        onView(withId(R.id.scrollView))
+                .inRoot(withDecorView(not(is(rule.getActivity().getWindow().getDecorView()))))
+                .perform(swipeDown());
         onView(withId(R.id.view)).check(matches(withLastNon0TranslationY(lessThan(0f))));
     }
 }
