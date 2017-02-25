2f36f15075bd2819dfb33711bf459244498bd0c9
kubode <kubode@team-lab.com>
2017-02-26 00:16:39 +0900

    Fix build.

--- core/src/androidTest/java/com/github/kubode/wiggle/WiggleHelperTest.java
+++ core/src/androidTest/java/com/github/kubode/wiggle/WiggleHelperTest.java
@@ -2,7 +2,6 @@ package com.github.kubode.wiggle;
 
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.WindowManager;
 
 import com.github.kubode.wiggle.test.R;
 
@@ -16,9 +15,11 @@ import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.swipeDown;
 import static android.support.test.espresso.action.ViewActions.swipeUp;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static com.github.kubode.wiggle.MyMatchers.withLastNon0TranslationY;
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
 
 @RunWith(AndroidJUnit4.class)
@@ -29,26 +30,22 @@ public class WiggleHelperTest {
 
     @Before
     public void setUp() {
-        rule.getActivity().runOnUiThread(new Runnable() {
-            public void run() {
-                rule.getActivity().getWindow().addFlags(
-                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-                                | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-                                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-            }
-        });
         onView(withId(R.id.view)).perform(scrollTo());
     }
 
     @Test
     public void lastNon0TranslationYGreaterThan0WhenSwipeUp() {
-        onView(withId(R.id.scrollView)).perform(swipeUp());
+        onView(withId(R.id.scrollView))
+                .inRoot(withDecorView(is(rule.getActivity().getWindow().getDecorView())))
+                .perform(swipeUp());
         onView(withId(R.id.view)).check(matches(withLastNon0TranslationY(greaterThan(0f))));
     }
 
     @Test
     public void lastNon0TranslationYLessThan0WhenSwipeDown() {
-        onView(withId(R.id.scrollView)).perform(swipeDown());
+        onView(withId(R.id.scrollView))
+                .inRoot(withDecorView(is(rule.getActivity().getWindow().getDecorView())))
+                .perform(swipeDown());
         onView(withId(R.id.view)).check(matches(withLastNon0TranslationY(lessThan(0f))));
     }
 }
